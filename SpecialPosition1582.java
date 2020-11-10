de690a30818350c322b1645d10989a68499c3b5e
Bazingamn <1804319795@qq.com>
2020-11-10 13:04:30 +0800

    modify 1582

---
+++ SpecialPosition1582.java
@@ -0,0 +1,17 @@
+public class SpecialPositionsInABinaryMatrix {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution1582 {
+    public int numSpecial(int[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j] == 1){
+                    
+                }
+            }
+        }
+    }
+}
