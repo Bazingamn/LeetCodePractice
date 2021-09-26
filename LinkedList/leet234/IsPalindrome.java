55fceb047e73c3432020ef0ea229f78ca208844e
Bruce Wang <1804319795@qq.com>
2021-09-26 18:42:01 +0800

    add 725

--- LinkedList/leet234/IsPalindrome.java
+++ LinkedList/leet234/IsPalindrome.java
@@ -76,9 +76,70 @@ class Solution1 {
  * 虽然不需要恢复也能通过测试用例，但是使用该函数的人通常不希望链表结构被更改。
  *
  * 这样可以避免使用O（n）的额外空间
+ *
+ * 流程：
+ * 1、找到前半部分链表的尾节点。
+ * 2、反转后半部分链表。
+ * 3、判断是否回文。
+ * 4、恢复链表。
+ * 5、返回结果。
+ *
+ * 执行步骤一，我们可以计算链表节点的数量，然后遍历链表找到前半部分的尾节点。
+ * 我们也可以使用快慢指针在一次遍历中找到：慢指针一次走一步，快指针一次走两步，快慢指针同时出发。当快指针移动到链表的末尾时，慢指针恰好到链表的中间。通过慢指针将链表分为两部分。
+ * 若链表有奇数个节点，则中间的节点应该看作是前半部分。
+ * 步骤二可以使用「206. 反转链表」问题中的解决方法来反转链表的后半部分。
+ * 步骤三比较两个部分的值，当后半部分到达末尾则比较完成，可以忽略计数情况中的中间节点。
+ * 步骤四与步骤二使用的函数相同，再反转一次恢复链表本身。
+ *
  */
 class Solution2 {
     public boolean isPalindrome(ListNode head) {
-        return false;
+        if (head == null){
+            return true;
+        }
+
+        // 找到前半部分链表的尾节点并反转后半部分链表
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        //判断是否回文
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2!= null){
+            if (p1.val != p2.val){
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        //还原链表并返回结果
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    //反转链表
+    ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    //找到前半部分链表的尾节点并返回
+    private ListNode endOfFirstHalf(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
---
+++ LinkedList/leet725/SplitListToParts.java
@@ -0,0 +1,36 @@
+package LinkedList.leet725;
+
+/**
+ * 给你一个头结点为 head 的单链表和一个整数 k ，请你设计一个算法将链表分隔为 k 个连续的部分。
+ *
+ * 每部分的长度应该尽可能的相等：任意两部分的长度差距不能超过 1 。这可能会导致有些部分为 null 。
+ *
+ * 这 k 个部分应该按照在链表中出现的顺序排列，并且排在前面的部分的长度应该大于或等于排在后面的长度。
+ *
+ * 返回一个由上述 k 部分组成的数组。
+ *
+ */
+public class SplitListToParts {
+
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+
+    }
+}
